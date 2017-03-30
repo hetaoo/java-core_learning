7e6d538381e40547fd7f8a189c5b7c7b637aea73
= <=>
2017-03-30 18:34:14 +0800

    add code

--- src/org/javacore/thread/base/yield/MyThread.java
+++ src/org/javacore/thread/base/yield/MyThread.java
@@ -3,5 +3,17 @@ package org.javacore.thread.base.yield;
 /**
  * Created by hetaoo on 2017/3/30.
  */
-public class MyThread {
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        long beginTime = System.currentTimeMillis();
+        int count = 0;
+        for (int i=0;i<50000;i++) {
+            //Thread.yield();
+            count = count + ++i;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("用时： " + (endTime - beginTime));
+    }
 }
--- src/org/javacore/thread/base/yield/Run.java
+++ src/org/javacore/thread/base/yield/Run.java
@@ -4,4 +4,9 @@ package org.javacore.thread.base.yield;
  * Created by hetaoo on 2017/3/30.
  */
 public class Run {
+
+    public static void main(String[] args){
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
 }
